14c158f755e11d8b3cc48775f263a21d1dc92c97
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-09-29 00:08:13 +0200

    add Menu Item Form UI

--- src/pages/AddMenuItem.js
+++ src/pages/AddMenuItem.js
@@ -6,6 +6,37 @@ const AddMenuItem = () => {
             <section className="main-head">
                 <h3>Add Menu Item</h3>
             </section>
+            <form>
+                <section className="form-section">
+                    <div className="form-row">
+                        <label>Type</label>
+                        <select>
+                            <option value='Side'>Side</option>
+                            <option value='Main Course'>Main Course</option>
+                        </select>
+                    </div>
+                    <div className="form-row">
+                        <label>Name</label>
+                        <input type="text" />
+                    </div>
+                    <div className="form-row">
+                        <label>Price</label>
+                        <input type="number" />
+                    </div>
+                    <div className="form-row">
+                        <label>Photo</label>
+                        <div className="myLabel">
+                            <div className="custom-file-input">
+                                <input type="file" />
+                                <button>Choose photo</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <button>Save Item</button>
+                    </div>
+                </section>
+            </form>
         </main>
     )
 }
