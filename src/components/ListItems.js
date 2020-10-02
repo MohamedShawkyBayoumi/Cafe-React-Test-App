759f7b85d36f8313d73ae30839ee0d14688b0b1a
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-10-02 11:33:45 +0200

    add ListItem component to list item cards

---
+++ src/components/ListItems.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ItemCard from '../components/ItemCard';
+import { useQuery, gql } from '@apollo/client';
+
+const MENU_LIST = gql`
+    query GetMenuItems {
+        items {
+            # id
+            type
+            name
+            price
+            photo
+        }
+    }
+`;
+
+function ListItems() {
+    const { loading, error, data } = useQuery(MENU_LIST);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <section className="cards-wrapper">
+            {data.items.length > 0 && data.items.map((item, index) => (
+                <ItemCard key={index} {...item} />
+            ))}
+        </section>
+    )
+}
+
+export default ListItems;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,25 +1,7 @@
 import React from 'react';
-import ItemCard from '../components/ItemCard';
-import { useQuery, gql } from '@apollo/client';
-
-const MENU_LIST = gql`
-    query GetMenuItems {
-        items {
-            # id
-            type
-            name
-            price
-            photo
-        }
-    }
-`;
+import ListItems from '../components/ListItems';
 
 const Menu = ({ history }) => {
-    const { loading, error, data } = useQuery(MENU_LIST);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-
     return (
         <main>
             <section className="main-head">
@@ -28,11 +10,7 @@ const Menu = ({ history }) => {
                     <button onClick={() => history.push('/AddMenuItem')}>Add menu item</button>
                 </div>
             </section>
-            <section className="cards-wrapper">
-                {data.items.length > 0 && data.items.map((item, index) => (
-                    <ItemCard key={index} {...item} />
-                ))}
-            </section>
+            <ListItems />
         </main>
     )
 }
